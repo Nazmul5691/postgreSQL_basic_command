c4859ebf0afdff4fa60f8e73f31f454869c06307
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-05-16 22:56:41 +0600

    use distinct

--- create.sql
+++ create.sql
@@ -34,3 +34,36 @@ VALUES
 
 
 SELECT * FROM students;
+
+
+
+
+CREATE TABLE students2(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    age int,
+    grade CHAR(2),
+    course VARCHAR(50),
+    email VARCHAR(50),
+    dob DATE,
+    blood_group VARCHAR(5),
+    country VARCHAR(20)
+)
+
+
+insert INTO students2(first_name, last_name, age, grade, course, email, dob, blood_group, country)
+VALUES
+('Jhon', 'Doe', 20, 'A+', 'Math', 'jhonDoe@gmail.com', '2024-01-15', 'O+', 'Canada'),
+('Alice', 'Smith', 22, 'B+', 'Physics', 'alice.smith@email.com', '2023-09-10', 'A-', 'USA'),
+('Robert', 'Brown', 19, 'C', 'Chemistry', NULL, '2022-06-25', 'B+', 'Canada'),
+('Emily', 'Johnson', 21, 'D', 'Biology', 'emily.johnson@email.com', '2023-12-01', 'AB+', 'USA'),
+('David', 'Williams', 23, 'B-', 'History', 'david.williams@email.com', '2024-03-20', 'O-', 'Germany'),
+('Sophia', 'Miller', 20, 'A+', 'English', NULL, '2023-05-18', 'A+', 'Germany'),
+('James', 'Davis', 22, 'B-', 'Economics', 'james.davis@email.com', '2022-11-11', 'B-', 'Italy'),
+('Olivia', 'Garcia', 18, 'A+', 'Computer Science', 'olivia.garcia@email.com', '2024-02-27', 'O+', 'USA'),
+('Liam', 'Martinez', 24, 'B+', 'Art', 'liam.martinez@email.com', '2023-01-30', 'AB-', 'Mexico'),
+('Emma', 'Anderson', 20, 'C+', 'Psychology', NULL, '2022-08-05', 'A-', 'Japan')
+
+
+SELECT * FROM students2;
---
+++ select_with_where_logical_and_comparison_operateor.sql
@@ -0,0 +1,5 @@
+SELECT * FROM students2;
+
+SELECT * FROM students2 ORDER BY dob ASC;
+
+SELECT DISTINCT country FROM students2;        -- only unique country
