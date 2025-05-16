90f8f2525d1bbdc6280003304cd9e718b7128e2d
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-05-16 22:37:42 +0600

    add select

--- select.sql
+++ select.sql
@@ -5,6 +5,25 @@ SELECT email FROM students;
 
 SELECT email, age FROM students;
 
-SELECT email FROM students;
+SELECT email, age, grade FROM students;
+
+SELECT email as student_email FROM students;       -- just only when see output
+
+SELECT email as "Student Email" FROM students;     -- just only when see output
+
+
+SELECT email as "Student Email", age FROM students;     -- just only when see output
+
+
+SELECT * FROM students ORDER BY first_name ASC;         -- sorting 
+
+SELECT * FROM students ORDER BY first_name DESC;
+
+SELECT * FROM students ORDER BY age DESC;
+
+SELECT * FROM students ORDER BY dob ASC;
+
+
+
 
 
