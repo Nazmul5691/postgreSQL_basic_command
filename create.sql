7399ef882a1965f76e42c469d2bebc14b4567247
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-05-16 22:30:04 +0600

    create table with multiple values

--- create.sql
+++ create.sql
@@ -3,3 +3,34 @@ CREATE TABLE student(
     user_name VARCHAR(20),
     age INTEGER
 )
+
+
+CREATE TABLE students(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    age int,
+    grade CHAR(2),
+    course VARCHAR(50),
+    email VARCHAR(50),
+    dob DATE,
+    blood_group VARCHAR(5),
+    country VARCHAR(20)
+)
+
+
+insert INTO students(first_name, last_name, age, grade, course, email, dob, blood_group, country)
+VALUES
+('Jhon', 'Doe', 20, 'A+', 'Math', 'jhonDoe@gmail.com', '2024-01-15', 'O+', 'Canada'),
+('Alice', 'Smith', 22, 'B+', 'Physics', 'alice.smith@email.com', '2023-09-10', 'A-', 'USA'),
+('Robert', 'Brown', 19, 'C', 'Chemistry', 'robert.brown@email.com', '2022-06-25', 'B+', 'UK'),
+('Emily', 'Johnson', 21, 'D', 'Biology', 'emily.johnson@email.com', '2023-12-01', 'AB+', 'Australia'),
+('David', 'Williams', 23, 'B-', 'History', 'david.williams@email.com', '2024-03-20', 'O-', 'Germany'),
+('Sophia', 'Miller', 20, 'A+', 'English', 'sophia.miller@email.com', '2023-05-18', 'A+', 'France'),
+('James', 'Davis', 22, 'B-', 'Economics', 'james.davis@email.com', '2022-11-11', 'B-', 'Italy'),
+('Olivia', 'Garcia', 18, 'A+', 'Computer Science', 'olivia.garcia@email.com', '2024-02-27', 'O+', 'Spain'),
+('Liam', 'Martinez', 24, 'B+', 'Art', 'liam.martinez@email.com', '2023-01-30', 'AB-', 'Mexico'),
+('Emma', 'Anderson', 20, 'C+', 'Psychology', 'emma.anderson@email.com', '2022-08-05', 'A-', 'Japan')
+
+
+SELECT * FROM students;
---
+++ select.sql
@@ -0,0 +1,10 @@
+SELECT * FROM students;
+
+
+SELECT email FROM students;
+
+SELECT email, age FROM students;
+
+SELECT email FROM students;
+
+
