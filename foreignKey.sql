d6bc6b28840620ddd0c838197a9f97982227398a
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-05-18 21:13:37 +0600

    data insertion

--- foreignKey.sql
+++ foreignKey.sql
@@ -8,3 +8,31 @@ CREATE TABLE post(
     text TEXT,
     user_id INTEGER REFERENCES "user"(id)
 )
+
+
+INSERT INTO "user"(username)
+    VALUES ('akash'),('batash'), ('nodi'), ('sagor')
+
+SELECT * FROM "user"
+
+
+INSERT into post(text, user_id)
+    VALUES
+    ('akash amr', 1),
+    ('batsh tmr', 2),
+    ('nodi amr', 3),
+    ('sagor tmr', 4)
+
+SELECT * FROM post
+
+
+INSERT into post(text, user_id) VALUES('test', 2)
+
+
+ALTER TABLE post
+    alter COLUMN user_id set NOT NULL;
+
+
+
+INSERT into post
+    VALUES('test')
