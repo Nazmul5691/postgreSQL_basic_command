2e880d70ff84a4dff23ff39754d8896984f4f414
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-05-22 21:48:50 +0600

    indexing

--- indexing.sql
+++ indexing.sql
@@ -12,3 +12,15 @@ ON employees (employee_name);
 
 
 SHOW data_directory;
+
+
+
+
+CREATE INDEX idx_employees_employee_name
+ON employees USING HASH (employee_name);
+
+CREATE INDEX idx_employees_employee_name
+ON employees USING GIN (employee_name);
+
+CREATE INDEX idx_employees_employee_name
+ON employees USING GIST (employee_name);
