4aa371913cd5b08a12dce3e59e4a042e1cc726d5
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-05-22 20:11:01 +0600

    procedure with variable

--- procedures.sql
+++ procedures.sql
@@ -17,3 +17,25 @@ $$
 CALL remove_emp();
 
 SELECT * FROM employees;
+
+
+
+
+--use variable
+CREATE Procedure remove_emp_var()
+LANGUAGE plpgsql
+AS
+$$
+    DECLARE
+      test_var INT;
+    BEGIN
+        SELECT employee_id INTO test_var FROM employees WHERE employee_id = 27;
+        DELETE FROM employees WHERE employee_id = test_var;
+    END
+$$
+
+CALL remove_emp_var();
+
+SELECT * FROM employees;
+
+
