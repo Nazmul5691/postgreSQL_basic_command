4021812300c1c760f9c1901985df19ea8ba3c804
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-05-22 20:14:14 +0600

    procedure with parmeters

--- procedures.sql
+++ procedures.sql
@@ -39,3 +39,22 @@ CALL remove_emp_var();
 SELECT * FROM employees;
 
 
+--use parameters
+CREATE Procedure remove_emp_id(p_emp_id INT)
+LANGUAGE plpgsql
+AS
+$$
+    DECLARE
+      test_var INT;
+    BEGIN
+        SELECT employee_id INTO test_var FROM employees WHERE employee_id = p_emp_id;
+        DELETE FROM employees WHERE employee_id = test_var;
+    END
+$$
+
+CALL remove_emp_id(26);
+
+
+SELECT * FROM employees;
+
+
